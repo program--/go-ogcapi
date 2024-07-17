a9427b0f9e6a28faa90300d28231798785b7f266
Tyler Brittain <tbrittain100@gmail.com>
2024-07-16 21:31:04 -0400

    feat: placeholder for static pages

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,32 @@ import (
 	"net/http"
 )
 
+const staticLocation = "./../static"
+
 func main() {
 	const port = ":8080"
-	log.Printf("Serving on port: %s\n", port)
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir(staticLocation))
 
-	// consider switching from default mux when we want to add middleware
-	log.Fatal(http.ListenAndServe(port, nil))
+	mux.Handle("/", fs)
+	mux.HandleFunc("/apiDefinition", apiDefinitionHandler)
+	mux.HandleFunc("/conformance", conformanceHandler)
+
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func apiDefinitionHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := staticLocation + "/apiDefinition.html"
+	http.ServeFile(w, r, filePath)
+}
+
+func conformanceHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := staticLocation + "/conformance.html"
+	http.ServeFile(w, r, filePath)
 }
